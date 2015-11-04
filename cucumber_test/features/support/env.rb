c1a6fb1aa52180c02db380a725e0db371fabff2f
Maik Zeyen <m.zeyen@epages.com>
2015-11-04 11:44:35 +0100

    Setp for: Anthony Eden - Build and Test APIs with Ruby and Cucumber

---
+++ cucumber_test/features/support/env.rb
@@ -0,0 +1,5 @@
+# include the file that hast the ruby steps
+require 'cucumber/rest_api'
+
+#Add base url so that it is globally accessible
+$SERVER_PATH = "http://admin:admin@agrohmann-vm3.intern.epages.de/epages/DemoShop.rest/api"
